a10735396619e798024cb71b6b5f983bd1db4a65
Richard Agyei <richard.agyei@gmail.com>
2022-09-22 20:14:55 -0500

    Cleanup

--- src/main/kotlin/com/richard/agyei/Todo.kt
+++ src/main/kotlin/com/richard/agyei/Todo.kt
@@ -1,18 +1,14 @@
 package com.richard.agyei
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 data class Todo(
-    @JsonProperty("userId")
-    val userId: Int,
     val id: String,
     val title: String,
     val completed: Boolean
-) {
-}
+)
 
 interface TodoApi {
 
